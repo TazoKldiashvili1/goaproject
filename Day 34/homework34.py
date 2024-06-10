633f8b85480f554348d7c206a6b4d3836d8612c1
Tazo1Kldiashvili <sanswak47@gmail.com>
2024-06-10 21:20:39 +0400

    second commit

---
+++ Day 34/homework34.py
@@ -0,0 +1,3 @@
+goa_13=["erekle yaragozishvili,andria qorchilava, luka natroshvili,mate kotorashvili,luka chaxvashvili,luxumi zurabiani,davit cixelashvili,giviko yavrishvili,saba lomsadze,nika kuchashbvili,deme biginashvili,mariam zardiashvili,toma omnashvili"]
+
+print(goa_13[5:15])
---
+++ Day 34/homework34_2.py
@@ -0,0 +1,9 @@
+Alphabet =list("abcdefghijklmnopqrstuvwxyznowiknowmyABC")
+
+letter_a = Alphabet[0]
+letter_h = Alphabet[7]
+letter_n = Alphabet[26]  
+
+Final_Alphabet= letter_a + letter_h +letter_n
+
+print(Final_Alphabet)
